643fea9c3160368c3a926b061f0096ee771108f9
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2025-01-03 22:25:49 +0300

    Conditionals and Booleans

--- index.php
+++ index.php
@@ -5,14 +5,30 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Demo</title>
+  <style>
+    body {
+      display: grid;
+      place-items: center;
+      height: 100vh;
+      margin: 0;
+      font-family: sans-serif;
+    }
+  </style>
 </head>
 
 <body>
+  <?php
+  $name = "Dark Matter";
+  $read = true;
+
+  if ($read) {
+    $message = "You have read \"$name.\"";
+  } else {
+    $message = "You have NOT read \"$name.\"";
+  }
+  ?>
   <h1>
-    <?php
-    $greeting = "Hello";
-    echo "$greeting Everybody!";
-    ?>
+    <?= $message; ?>
   </h1>
 </body>
 
