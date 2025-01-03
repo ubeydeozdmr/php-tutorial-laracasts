2b2424faeaea74b737a106f28dc8106dea7a4756
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2025-01-03 23:33:14 +0300

    Associative Arrays

--- index.php
+++ index.php
@@ -12,15 +12,26 @@
 
   <?php
   $books = [
-    "Do Androids Dream of Electric Sheep",
-    "The Langoliers",
-    "Hail Mary",
+    [
+      'name' => 'Do Androids Dream of Electric Sheep',
+      'author' => 'Philip K. Dick',
+      'purchaseUrl' => 'http://example.com'
+    ],
+    [
+      'name' => 'Project Hail Mary',
+      'author' => 'Andy Weir',
+      'purchaseUrl' => 'http://example.com'
+    ]
   ];
   ?>
 
   <ul>
     <?php foreach ($books as $book) : ?>
-      <li><?= $book ?></li>
+      <li>
+        <a href="<?= $book['purchaseUrl'] ?>">
+          <?= $book['name'] ?>
+        </a>
+      </li>
     <?php endforeach; ?>
   </ul>
 </body>
